41e4cc2c8355d5860bd20ce90e6d904731f032e2
gustavo sulzer Roquette <gsroquette@gmail.com>
2024-10-17 21:45:26 +0700

    gugugugugu

--- server.js
+++ server.js
@@ -1,28 +1,24 @@
-// Carregar módulos necessários
+// Carregar módulos necessários 
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Middleware para parsing de JSON
-app.use(express.json());
+// Configurar o Express para servir arquivos estáticos diretamente da raiz do projeto
+app.use(express.static(__dirname));
 
-// Rota principal para verificar se a API está funcionando
-app.get('/', (req, res) => {
-    res.send('API está funcionando');
-});
-
-// Rota para fornecer a configuração do Firebase
+// Endpoint para servir a configuração do Firebase
 app.get('/firebase-config', (req, res) => {
     res.json({
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        databaseURL: process.env.FIREBASE_DATABASE_URL,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
-        measurementId: process.env.FIREBASE_MEASUREMENT_ID
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID,
+        appId: process.env.APP_ID,
+        measurementId: process.env.MEASUREMENT_ID
     });
 });
 
