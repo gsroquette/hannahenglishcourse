226b70e662e6d8f4fd097fbd8d13d2daa86f6557
gustavo sulzer Roquette <gsroquette@gmail.com>
2024-10-18 15:03:56 +0700

    Update script.js

--- Level1/Unit4/script.js
+++ Level1/Unit4/script.js
@@ -3,12 +3,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const auth = firebase.auth();
 
     const activities = [
-        { id: 1, name: "StoryCards", path: "../Unit4/StoryCards/index.html", img: "../../imagens/botoes/storycards_button.png", unlocked: false },
-        { id: 2, name: "Flashcards", path: "../Unit4/Flashcards/index.html", img: "../../imagens/botoes/flashcards_button.png", unlocked: false },
-        { id: 3, name: "Flashcards2", path: "../Unit4/Flashcards2/index.html", img: "../../imagens/botoes/flashcards_button.png", unlocked: false },
-        { id: 4, name: "Flashcards3", path: "../Unit4/Flashcards3/index.html", img: "../../imagens/botoes/flashcards_button.png", unlocked: false },
-        { id: 5, name: "QUIZ", path: "../Unit4/QUIZ/index.html", img: "../../imagens/botoes/quiz_button.png", unlocked: false },
-    ];
+    { id: 1, name: "StoryCards", path: "../Unit4/StoryCards/index.html?fase=1", img: "../../imagens/botoes/storycards_button.png", unlocked: false },
+    { id: 2, name: "Flashcards", path: "../Unit4/Flashcards/index.html?fase=2", img: "../../imagens/botoes/flashcards_button.png", unlocked: false },
+    { id: 3, name: "Flashcards2", path: "../Unit4/Flashcards2/index.html?fase=3", img: "../../imagens/botoes/flashcards_button.png", unlocked: false },
+    { id: 4, name: "Flashcards3", path: "../Unit4/Flashcards3/index.html?fase=4", img: "../../imagens/botoes/flashcards_button.png", unlocked: false },
+    { id: 5, name: "QUIZ", path: "../Unit4/QUIZ/index.html?fase=5", img: "../../imagens/botoes/quiz_button.png", unlocked: false }
+];
 
     const mapContainer = document.getElementById('mapContainer');
     const svgContainer = document.getElementById('linesSvg');
