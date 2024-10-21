b76f3d999b4c73c4760f6cf5ed9aec8f57912a6d
gustavo sulzer Roquette <gsroquette@gmail.com>
2024-10-21 22:37:31 +0700

    Update script4.js

--- Level1/Unit1/script4.js
+++ Level1/Unit1/script4.js
@@ -3,11 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const auth = firebase.auth();
 
     const activities = [
-        { id: 1, name: "StoryCards", path: "../Unit1/StoryCards/index.html?fase=1", img: "../../imagens/botoes/storycards_button.png", unlocked: false },
-        { id: 2, name: "Flashcards", path: "../Unit1/Flashcards/index.html?fase=2", img: "../../imagens/botoes/flashcards_button.png", unlocked: false },
-        { id: 3, name: "Flashcards2", path: "../Unit1/Flashcards2/index.html?fase=3", img: "../../imagens/botoes/flashcards_button.png", unlocked: false },
-        { id: 4, name: "Flashcards3", path: "../Unit1/Flashcards3/index.html?fase=4", img: "../../imagens/botoes/flashcards_button.png", unlocked: false },
-        { id: 5, name: "QUIZ", path: "../Unit1/QUIZ/index.html?fase=5", img: "../../imagens/botoes/quiz_button.png", unlocked: false }
+        { id: 14, name: "WordSearch", path: "../Unit1/WordSearch/index.html?fase=14", img: "../../imagens/botoes/wordsearch_button.png", unlocked: false },
+        { id: 15, name: "WordSearch2", path: "../Unit1/WordSearch2/index.html?fase=15", img: "../../imagens/botoes/wordsearch_button.png", unlocked: false },
+        { id: 16, name: "WordSearch3", path: "../Unit1/WordSearch3/index.html?fase=16", img: "../../imagens/botoes/wordsearch_button.png", unlocked: false },
+        { id: 17, name: "Flashcards3", path: "../Unit1/Flashcards3/index.html?fase=1", img: "../../imagens/botoes/flashcards_button.png", unlocked: false },
+        { id: 18, name: "QUIZ", path: "../Unit1/QUIZ/index.html?fase=18", img: "../../imagens/botoes/quiz_button.png", unlocked: false }
     ];
 
     const mapContainer = document.getElementById('mapContainer');
