005187c7106ef671a5d3c0e518775a037ca3c0cf
gustavo sulzer Roquette <gsroquette@gmail.com>
2024-11-06 10:14:04 +0700

    wordtest2e3

--- Level1/Unit1/script6.js
+++ Level1/Unit1/script6.js
@@ -13,7 +13,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const activities = [
         { id: 24, name: "Test", path: "../Unit1/Test/index.html?fase=24", img: "../../imagens/test.png", unlocked: false },
         { id: 25, name: "Test1", path: "../Unit1/Testlistening/index.html?fase=25", img: "../../imagens/test.png", unlocked: false },   
-        { id: 26, name: "Test2", path: "../Unit1/Testword/index.html?fase=last", img: "../../imagens/test.png", unlocked: false },                   
+        { id: 26, name: "Test2", path: "../Unit1/Testword/index.html?fase=26", img: "../../imagens/test.png", unlocked: false },  
+        { id: 27, name: "Test3", path: "../Unit1/Testword1/index.html?fase=27", img: "../../imagens/test.png", unlocked: false },             
+        { id: 28, name: "Test4", path: "../Unit1/Testword2/index.html?fase=last", img: "../../imagens/test.png", unlocked: false },                              
     ];
 
      // Fechar o dropdown ao clicar fora dele
