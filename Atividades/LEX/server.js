e16533e02666cdb8c8b40a04b024f4e8ac521414
gustavo sulzer Roquette <gsroquette@gmail.com>
2024-12-13 15:00:58 +0700

    Update server.js

--- Atividades/LEX/server.js
+++ Atividades/LEX/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -7,11 +8,18 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Defina o caminho correto do index
+const indexPath = path.join(__dirname, '../../Atividades/LEX/index.html');
+
 // Configure a OpenAI API Key
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
+
+app.get('/', (req, res) => {
+    res.sendFile(indexPath); // Envia o arquivo index.html como resposta
+});
 
 app.post('/chat', async (req, res) => {
     const userMessage = req.body.message;
