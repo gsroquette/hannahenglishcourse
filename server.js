982ef3fd8a89677701fd17efae4ab1a71cb23a68
gustavo sulzer Roquette <gsroquette@gmail.com>
2025-02-01 01:49:49 +0630

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') }); // Carrega variáveis do .env corretamente
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
 
 const express = require('express');
 const bodyParser = require('body-parser');
