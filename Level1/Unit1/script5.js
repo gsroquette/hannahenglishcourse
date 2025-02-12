331b0c6b2f173f75477f0fce0a63acfec8a8ff63
gustavo sulzer Roquette <gsroquette@gmail.com>
2025-02-12 16:11:36 +0630

    dialogo

--- Level1/Unit1/script5.js
+++ Level1/Unit1/script5.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function() {
 const activities = [
     { id: 21, name: "Grammar2", path: `/Atividades/Grammar2/index.html?level=${currentLevel}&unit=${currentUnit}&fase=21`, img: "../../imagens/botoes/grammar_button.png", unlocked: false },
     { id: 22, name: "DataExerciciosdaGramatica2", path: `/Atividades/DataExerciciosdaGramatica2/index.html?level=${currentLevel}&unit=${currentUnit}&fase=22`, img: "../../imagens/botoes/exercises_button.png", unlocked: false },
-    { id: 23, name: "Grammar3", path: `/Atividades/Grammar3/index.html?level=${currentLevel}&unit=${currentUnit}&fase=23`, img: "../../imagens/botoes/grammar_button.png", unlocked: false },
+    { id: 23, name: "GrammarDialogo", path: `/Atividades/GrammarDialogo/index.html?level=${currentLevel}&unit=${currentUnit}&fase=23`, img: "../../imagens/botoes/grammar_button.png", unlocked: false },
     { id: 24, name: "QUIZ2", path: `/Atividades/QUIZ2/index.html?level=${currentLevel}&unit=${currentUnit}&fase=24`, img: "../../imagens/botoes/quiz_button.png", unlocked: false },
     { id: 25, name: "Diálogo", path: `/Atividades/Dialogo/index.html?level=${currentLevel}&unit=${currentUnit}&fase=25`, img: "../../imagens/botoes/dialogo_button.png", unlocked: false },
     { id: 26, name: "Speak", path: `/Atividades/Speak/index.html?level=${currentLevel}&unit=${currentUnit}&fase=26`, img: "../../imagens/botoes/speak_button.png", unlocked: false },
