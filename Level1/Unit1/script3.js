568c6cd43f35c116e261e43e2a35cf2c97545a4b
Hannah <gsroquette@gmail.com>
2025-04-06 23:52:41 +0700

    dsdsdeeeeee

--- Level1/Unit1/script3.js
+++ Level1/Unit1/script3.js
@@ -12,8 +12,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Extraindo Level e Unit da URL atual
     const urlPathParts = window.location.pathname.split('/');
-    const currentLevel = urlPathParts[1]; // Ex: "Level1"
-    const currentUnit = urlPathParts[2]; // Ex: "Unit1"
+    const capitalizeFirstLetter = str => str.charAt(0).toUpperCase() + str.slice(1);
+    const currentLevel = capitalizeFirstLetter(urlPathParts[1]);
+    const currentUnit = capitalizeFirstLetter(urlPathParts[2]);
 
    const activities = [
     { id: 1012, name: "Grammar", path: `/Atividades/Grammar/index.html?level=${currentLevel}&unit=${currentUnit}&fase=1012`, img: "../../imagens/botoes/grammar_button.png", unlocked: false },
@@ -82,8 +83,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Função para carregar o progresso do usuário
    function loadUserProgress(userId, userAvatar, userRole) {
-    const progressPath = `/usuarios/${userId}/progresso/Level1/Unit1`;
-    console.log(`Buscando progresso em: ${progressPath}`);
+   const progressPath = `/usuarios/${userId}/progresso/${currentLevel}/${currentUnit}`;
+   console.log(`Buscando progresso em: ${progressPath}`);
 
     if (userRole === 'proprietario' || userRole === 'professor') {
         activities.forEach(activity => activity.unlocked = true);
