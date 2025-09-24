95388f6acf96ea56d883eb6646efff873f81b8c8
Hannah <gsroquette@gmail.com>
2025-09-24 17:36:51 -0300

    botoes

--- Level0/Unit1/atividadesPorTrecho.js
+++ Level0/Unit1/atividadesPorTrecho.js
@@ -6,8 +6,9 @@ function getActivitiesByTrecho(trecho, level, unit) {
             return [
                 { id: 1, name: "StoryCards", path: `/Atividades/StoryCards/index.html?level=${level}&unit=${unit}&fase=1`, img: "../../imagens/botoes/storycards_button.png", unlocked: false },
                 { id: 2, name: "Flashcards", path: `/Atividades/Flashcards/index.html?level=${level}&unit=${unit}&fase=2`, img: "../../imagens/botoes/flashcards_button.png", unlocked: false },
-                { id: 3, name: "Flashcards2", path: `/Atividades/Flashcards2/index.html?level=${level}&unit=${unit}&fase=3`, img: "../../imagens/botoes/flashcards_button.png", unlocked: false },   
-                { id: 4, name: "Fill in the Blanks", path: `/Atividades/Fill%20in%20the%20Blanks/index.html?level=${level}&unit=${unit}&fase=4`, img: "../../imagens/botoes/fillintheblanks_button.png", unlocked: false },
+                { id: 3, name: "Flashcards2", path: `/Atividades/Flashcards2/index.html?level=${level}&unit=${unit}&fase=3`, img: "../../imagens/botoes/flashcards_button.png", unlocked: false },
+                { id: 4, name: "Word Drop Express", path: `/Atividades/word%20drop%20express/index.html?level=${level}&unit=${unit}&fase=4`, img: "../../imagens/botoes/ballonGame.png", unlocked: false },     
+                { id: 5, name: "Perguntas Starter", path: `/Atividades/perguntas%20starter/index.html?level=${level}&unit=${unit}&fase=5`, img: "../../imagens/botoes/hannah_exercises.png", unlocked: false },
             ];
         case "trecho2":
             return [
